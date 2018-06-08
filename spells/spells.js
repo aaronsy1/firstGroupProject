4d7858cc21a8c634a635d30b9ebf7a9bcf9fcac0
Blake Mason <blake@dhcp-v079-013.mobile.uci.edu>
2018-06-07 21:27:29 -0700

    spell page string clean up

--- spells/spells.js
+++ spells/spells.js
@@ -24,7 +24,6 @@ $(document.body).on('click', '.btn-secondary', function () {
     total = parseInt(total);
     total = total + roll;
     $(this).siblings(':first').children().children().text(total);
-    console.log();
 })
 
 
@@ -42,28 +41,44 @@ $(document.body).on('click', '#spell-search', function () {
     //get user input
     var input = $('#spell-input').val();
     input.replace('/ /g', '+');
-    console.log(input);
 
     //get request for spell number
     axios.get('http://www.dnd5eapi.co/api/spells/?name=' + input)
         .then(function (response) {
-            console.log(response);
             var url = response.data.results[0].url;
             return axios.get(url);
         })
 
         //second request to get data
-        .then(function (response) {
-            console.log(response);
+        .then(function (response) {            
             var description = response.data.desc[0];
-            console.log(description);
-            var regExpr = /[^a-zA-Z0-9-. ]/g;
-            description = description.replace(regExpr, '\'');
-            console.log(description);
-            $('#spell-info-col').children().remove();
-            $('#spell-info-col').append('<div class="card mb-3"></h5><div class="card-body"><h5 class="card-title">' + input + '</h5><p>' + description + '</p></div></div>');
-            //text cleanup
 
+            //cleanup weird sybols
+            var regExpr = /[^a-zA-Z0-9-. ,;:"'()?]/g;
+            description = description.replace(regExpr, '\'');
+            description = description.replace(/'''/g, '\'');
+
+            //add to hmtml
+            $('#spell-info-col').children().remove();
+            $('#spell-info-col').append('<div class="card mb-3"></h5><div class="card-body" id="spell-description"><h5 class="card-title">' + input + '</h5><p>' + description + '</p></div></div>');
+
+            //add extra description info
+            if (response.data.desc.length > 1) {
+                for (i = 1; i < response.data.desc.length; i++) {
+                    description = response.data.desc[i];
+                    description = description.replace(regExpr, '\'');
+                    description = description.replace(/'''/g, '\'');
+                    $('#spell-description').append('<p>' + description + '</p>');
+                }
+            }
+
+            //add extra levels spell damage
+            if (response.data.higher_level) {
+                description = response.data.higher_level[0];
+                description = description.replace(regExpr, '\'');
+                description = description.replace(/'''/g, '\'');
+                $('#spell-description').append('<p>' + description + '</p>');
+            }
         })
 
         //error
