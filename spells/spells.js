7d5ee2b360d48b640263ef986401b17e95c2fe21
Blake Mason <blake@dhcp-v105-045.mobile.uci.edu>
2018-06-09 10:18:03 -0700

    Updated spell tool. Changed to github api. Roll animations. Styling. Dynamics search

--- spells/spells.js
+++ spells/spells.js
@@ -1,88 +1,203 @@
+//create spell array
+var url = "https://raw.githubusercontent.com/vorpalhex/srd_spells/master/spells.json";
+var spellArray = []
+$(document).ready(function () {
+	axios
+		.get(url)
+		.then(function (response) {
+			for (var i = 0; i < response.data.length; i++) {
+				var $spellFilter = $('#spell-filter');
+				$spellFilter
+					.append(
+						'<div class="col-12 col-sm-auto py-1 px-2 p-sm-2 text-center spell-shortcut" style="display: none"><button class="btn btn-light spell-button btn-block">' + response.data[i].name + '</button></div>'
+					)
+			}
+		})
+
+		.catch(function (error) {
+			console.log(error);
+		});
+});
+
 // dice click
-$(document.body).on('click', '.btn-secondary', function () {
-    //get dice value
-    var $dice = $(this).text();
-    var $diceDiv = $(this).parent();
+$(document.body).on("click", ".dice-button", function () {
+	//random number function
+	function getRndInteger(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 
-    //random number function
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+	//jquery variables
+	var $dice = $(this).text();
+	var $diceDiv = $(this).parent();
+	var $this = $(this);
 
-    //set dice roll and create on page
-    var roll = getRndInteger(1, $dice);
-    var $card = $('<div class="card mx-auto mt-2 roll"><div class="card-body p-1 text-center">' + roll + '</div></div>');
-    $(this).after($card);
+	//set dice roll html and animation
+	var $card = $(
+		'<div class="card mx-auto mt-2 roll animated bounceIn" style="z-index: -1"></div>'
+	);
+	var $cardBody = $('<div class="card-body p-1 text-center"></div>');
+	$this.after($card);
+	$card.append($cardBody);
+	var x = 0;
+	var intervalID = setInterval(function () {
+		$($cardBody).html(getRndInteger(1, $dice));
+		if (++x === 16) {
+			window.clearInterval(intervalID);
+		}
+	}, 30);
 
-    //calculate totals and update on page
-    var total = $(this).siblings(':first').children().children().text();
-    if (total === '-') {
-        total = 0;
-    } else {
-
-    }
-    total = parseInt(total);
-    total = total + roll;
-    $(this).siblings(':first').children().children().text(total);
-})
-
+	//calculate totals and update on page
+	setTimeout(function () {
+		var total = $this
+			.siblings(":first")
+			.children()
+			.children()
+			.text();
+		if (total === "-") {
+			total = 0;
+		}
+		total = parseInt(total);
+		var roll = parseInt($card.text());
+		total = total + roll;
+		$this
+			.siblings(":first")
+			.children()
+			.children()
+			.text(total);
+	}, 505);
+});
 
 
 //reset dice click
-$(document.body).on('click', '#reset-button', function () {
-    $('.total').text('-');
-    $('.roll').remove();
-})
-
+$(document.body).on("click", "#reset-button", function () {
+	$(".total").text("-");
+	$(".roll").remove();
+});
 
 
 //search spell click
-$(document.body).on('click', '#spell-search', function () {
-    //get user input
-    var input = $('#spell-input').val();
-    input.replace('/ /g', '+');
+$(document.body).on("click", "#spell-search", function () {
+	var input = $("#spell-input").val();
+	input = input.trim();
+	input = input.toUpperCase();
+	axios
+		.get(url)
+		.then(function (response) {
+			$("#spell-info-col")
+				.children()
+				.remove();
+			var spellIndex;
+			for (i = 0; i < response.data.length; i++) {
+				if (response.data[i].name.toUpperCase() === input) {
+					spellIndex = i;
+					break;
+				}
+			}
 
-    //get request for spell number
-    axios.get('http://www.dnd5eapi.co/api/spells/?name=' + input)
-        .then(function (response) {
-            var url = response.data.results[0].url;
-            return axios.get(url);
-        })
+			var description = response.data[spellIndex].description;
+			var duration = response.data[spellIndex].duration;
+			var level = response.data[spellIndex].level;
+			var range = response.data[spellIndex].range;
+			var name = response.data[spellIndex].name;
+			var higherLevels = response.data[spellIndex].higher_levels;
+			$("#spell-info-col").append(
+				'<div class="card pt-2 animated fadeIn"></h5><div class="card-body" id="spell-description"><h5 class="card-title">' +
+				name +
+				"</h5></div></div>"
+			);
+			$("#spell-description").append("<div>Level: " + level + "</div>");
+			$("#spell-description").append("<div>Range: " + range + "</div>");
+			$("#spell-description").append("<p>Duration: " + duration + "</p>");
 
-        //second request to get data
-        .then(function (response) {            
-            var description = response.data.desc[0];
+			if (higherLevels != undefined) {
+				$("#spell-description").append("<p>" + description + "</p>");
+				$("#spell-description").append("<div>" + higherLevels + "</div>");
+			} else {
+				$("#spell-description").append("<div>" + description + "</div>");
+			}
+		})
 
-            //cleanup weird sybols
-            var regExpr = /[^a-zA-Z0-9-. ,;:"'()?]/g;
-            description = description.replace(regExpr, '\'');
-            description = description.replace(/'''/g, '\'');
+		.catch(function (error) {
+			console.log(error);
+		});
+});
 
-            //add to hmtml
-            $('#spell-info-col').children().remove();
-            $('#spell-info-col').append('<div class="card mb-3"></h5><div class="card-body" id="spell-description"><h5 class="card-title">' + input + '</h5><p>' + description + '</p></div></div>');
+var input = document.getElementById("spell-input");
 
-            //add extra description info
-            if (response.data.desc.length > 1) {
-                for (i = 1; i < response.data.desc.length; i++) {
-                    description = response.data.desc[i];
-                    description = description.replace(regExpr, '\'');
-                    description = description.replace(/'''/g, '\'');
-                    $('#spell-description').append('<p>' + description + '</p>');
-                }
-            }
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function (event) {
+	// Cancel the default action, if needed
+	event.preventDefault();
+	// Number 13 is the "Enter" key on the keyboard
+	if (event.keyCode === 13) {
+		// Trigger the button element with a click
+		document.getElementById("spell-search").click();
+	}
+});
 
-            //add extra levels spell damage
-            if (response.data.higher_level) {
-                description = response.data.higher_level[0];
-                description = description.replace(regExpr, '\'');
-                description = description.replace(/'''/g, '\'');
-                $('#spell-description').append('<p>' + description + '</p>');
-            }
-        })
 
-        //error
-        .catch(function (error) {
-            console.log(error);
-        });
+//dynamic filter
+function myFunction() {
+	$('.spell-shortcut').each(function () {
+		var $input = $('#spell-input').val().toUpperCase();
+		var $this = $(this);
+		if ($input === '') {
+			$this.css('display', 'none');
+		} else {
+			if ($this.text().toUpperCase().indexOf($input) > -1) {
+				$this.css('display', 'initial');
+			} else {
+				$this.css('display', 'none');
+			}
+		}
+	})
+}
+
+
+//spell button on click
+$(document.body).on("click", ".spell-button", function () {
+	var $this = $(this);
+	var spellName = $this.text().toUpperCase();
+	axios
+		.get(url)
+		.then(function (response) {
+			console.log(spellName)
+			$("#spell-info-col")
+				.children()
+				.remove();
+			var spellIndex;
+			for (i = 0; i < response.data.length; i++) {
+				if (response.data[i].name.toUpperCase() === spellName) {
+					spellIndex = i;
+					break;
+				}
+			}
+
+			var description = response.data[spellIndex].description;
+			var duration = response.data[spellIndex].duration;
+			var level = response.data[spellIndex].level;
+			var range = response.data[spellIndex].range;
+			var name = response.data[spellIndex].name;
+			var higherLevels = response.data[spellIndex].higher_levels;
+			$("#spell-info-col").append(
+				'<div class="card mt-1 animated fadeIn"></h5><div class="card-body" id="spell-description"><h5 class="card-title">' +
+				name +
+				"</h5></div></div>"
+			);
+			$("#spell-description").append("<div>Level: " + level + "</div>");
+			$("#spell-description").append("<div>Range: " + range + "</div>");
+			$("#spell-description").append("<p>Duration: " + duration + "</p>");
+
+			if (higherLevels != undefined) {
+				$("#spell-description").append("<p>" + description + "</p>");
+				$("#spell-description").append("<div>" + higherLevels + "</div>");
+			} else {
+				$("#spell-description").append("<div>" + description + "</div>");
+			}
+		})
+
+		.catch(function (error) {
+			console.log(error);
+		});
+
 })
